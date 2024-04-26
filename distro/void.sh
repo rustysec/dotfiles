0197b5196af6ca9f6cf13bc5c760996925ca749c
rustysec <russ@infocyte.com>
2024-04-26 09:23:55 -0700

    fonts

--- distro/void.sh
+++ distro/void.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
-sudo xbps-install -Sy zsh tmux wget curl dbus polkit pipewire wireplumber elogind seatd
-sudo xbps-install -Sy sway swaylock Waybar mako fuzzel kanshi foot
-sudo xbps-install -Sy xdg-desktop-portal-wlr xdg-desktop-portal-gtk mate-polkit
+sudo xbps-install -Sy \
+    zsh \
+    tmux \
+    wget \
+    curl \
+    dbus \
+    polkit \
+    pipewire \
+    wireplumber \
+    elogind
+    seatd \
+    sway \
+    swaylock \
+    Waybar \
+    mako \
+    fuzzel \
+    kanshi \
+    foot \
+    psutils \
+    xdg-desktop-portal-wlr \
+    xdg-desktop-portal-gtk \
+    mate-polkit \
+    font-hack-ttf \
+    nerd-fonts
 
 sudo usermod -a -G _seatd $(whoami)
