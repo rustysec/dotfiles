0fb8c5ad9c560dad3b7739cb74102f6229b162dc
rustysec <russ@infocyte>
2024-05-07 21:26:23 -0700

    nvim inlay hints

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,6 +36,7 @@ require('focus').setup({})
 require("ibl").setup()
 require('gitsigns').setup()
 require('mini.surround').setup()
+require('lsp-inlayhints').setup()
 
 local cmp = require('cmp')
 
@@ -142,20 +143,20 @@ lspconfig.nil_ls.setup({
     },
 })
 
--- require('lsp-inlayhints').setup({})
--- vim.api.nvim_create_augroup('LspAttach_inlayhints', {})
--- vim.api.nvim_create_autocmd('LspAttach', {
--- group = 'LspAttach_inlayhints',
--- callback = function(args)
---     if not (args.data and args.data.client_id) then
---         return
---     end
---
---     local bufnr = args.buf
---     local client = vim.lsp.get_client_by_id(args.data.client_id)
---     require('lsp-inlayhints').on_attach(client, bufnr, true)
--- end
--- })
+require('lsp-inlayhints').setup({})
+vim.api.nvim_create_augroup('LspAttach_inlayhints', {})
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = 'LspAttach_inlayhints',
+    callback = function(args)
+        if not (args.data and args.data.client_id) then
+            return
+        end
+
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        require('lsp-inlayhints').on_attach(client, bufnr, true)
+    end
+})
 
 vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
