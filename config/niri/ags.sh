85f5e511e24b6cbd378a6f1b493b8e0d6838bb55
rustysec <russ@infocyte.com>
2024-05-14 22:19:16 -0700

    adjusting niri and bars

--- config/niri/ags.sh
+++ config/niri/ags.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
-PATH=$PATH:~/.npm-global/bin
-ags -q 2>/dev/null || true
-killall -9 ags 2>/dev/null || true
-ags -c ~/.config/ags/config.js &
+
+function setup_bars() {
+    PATH=$PATH:~/.npm-global/bin
+    ags -q 2>/dev/null || true
+    ags &
+}
+
+setup_bars
+
+MONITORS=`niri msg outputs | grep Output | wc -l`
+
+while true
+do
+    CURRENT=`niri msg outputs | grep Output | wc -l`
+    if [[ $CURRENT -gt $MONITORS ]]; then
+        echo "New monitor config!"
+        setup_bars
+    fi
+
+    MONITORS=$CURRENT
+
+    sleep 3
+done
