59b1fb46bc28d296db3a7636edeba4134ae2822c
rustysec <russ@infocyte.com>
2024-10-16 22:59:26 -0700

    updating niri waybar

--- config/niri/waybar.sh
+++ config/niri/waybar.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-for file in `ls $HOME/.config/waybar/config.niri-*`; do
-    # waybar -c $file 2>&1 >/dev/null & 
-    waybar -c $file  & 
-done
+waybar -c ~/.config/waybar/config.niri & 
