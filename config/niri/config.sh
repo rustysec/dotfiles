2414c6cd8fe62d49336dcd7f22378c1d489a65cc
rustysec <russ@infocyte.com>
2024-10-21 08:31:15 -0700

    niri struts

--- config/niri/config.sh
+++ config/niri/config.sh
@@ -2,8 +2,9 @@
 
 dir=`dirname $0`
 hostname=`hostname`
-distro=`lsb_release -a | grep Description | awk '{print $2}' | tr A-Z a-z`
-cursor_theme='breeze_cursors'
+distro=`lsb_release -a 2>/dev/null | grep Description | awk '{print $2}' | tr A-Z a-z || "Unknown"`
+# cursor_theme='breeze_cursors'
+cursor_theme='Adwaita'
 
 cat $dir/config.kdl.base > ~/.config/niri/config.kdl
 cat $dir/config.kdl.$hostname >> ~/.config/niri/config.kdl 2>/dev/null
