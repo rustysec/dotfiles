6c77300a4f8313ed67ba95635b6966be21ae921d
rustysec <russ@infocyte.com>
2025-01-03 07:49:31 -0800

    swayfx updates

--- config/niri/pipewire.sh
+++ config/niri/pipewire.sh
@@ -5,7 +5,7 @@ then
     # systemd should handle this
     echo "systemd, do your thing"
 else
-    ITEMS=("pipewire" "pipewire-pulse" "wireplumber")
+    ITEMS=("pipewire")
 
     for item in ${ITEMS[@]}; do
         killall -9 $item 2>&1 >/dev/null
