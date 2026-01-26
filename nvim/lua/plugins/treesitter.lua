f01b617be2ad1371938e94c5a0019c41d3a5458b
rustysec <rustysec@gmail.com>
2026-01-26 12:26:56 -0800

    nvim: update new treesitter version

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,5 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function()
-        local configs = require("nvim-treesitter.configs")
-
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate'
 }
